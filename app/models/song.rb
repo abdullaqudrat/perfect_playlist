eb4d8419db5e8a8cb9123f93d738f4ae19b131bd
abdyqew <aqudrat630@gmail,com>
2018-09-04 21:56:52 -0600

    changes song show page to reflect playlists

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,9 @@ class Song < ApplicationRecord
   belongs_to :artist
   has_many :playlist_songs
   has_many :playlists, through: :playlist_songs
+
+  accepts_nested_attributes_for :artist
+
   validates_presence_of :title,
                         :length,
                         :bpm,
